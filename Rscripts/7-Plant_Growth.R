b3493ce0a85a6f6383c90dfd1d66a558879a6040
priyanka@laptop <priyanka@email.arizona.edu>
2022-02-24 17:51:26 -0700

    Shorten scripts using two for loops

--- Rscripts/7-Plant_Growth.R
+++ Rscripts/7-Plant_Growth.R
@@ -13,168 +13,65 @@ dim(chem) #120 46
 plant <- subset(chem, chem$Population != "Blank")
 
 #### Plot ELA, FvFm, PI_abs over days ####
-# NM_PL14_P Calculate mean for ELA, FvFm, PI_abs
-pl14.growth1 <- plant %>% 
-  filter(Population_type == "NM_PL14_P") %>%
-  group_by(Soil_type) %>%
-  summarize(ELA_value = mean(ELA_T0),
-            Fv_Fm_value = mean(Fv_Fm_T0),
-                      PI_abs_value = mean(PI_abs_T0))
 
-pl14.growth2 <- plant %>% 
-  filter(Population_type == "NM_PL14_P") %>%
-  group_by(Soil_type) %>%
-  summarize(ELA_value = mean(ELA_T1),
-            Fv_Fm_value = mean(Fv_Fm_T1),
-            PI_abs_value = mean(PI_abs_T1))
+pop <- unique(plant$Population_type)
+dat1 <- data.frame()
+dat2 <- data.frame()
+dat3 <- data.frame()
 
-pl14.growth3 <- plant %>% 
-  filter(Population_type == "NM_PL14_P") %>%
-  group_by(Soil_type) %>%
-  summarize(ELA_value = mean(ELA_T2),
-            Fv_Fm_value = mean(Fv_Fm_T2),
-            PI_abs_value = mean(PI_abs_T2))
+for(p in pop) {
+ plant_growth1 <- plant %>%
+    filter(Population_type == p) %>%
+    group_by(Soil_type) %>%
+    summarize(ELA_value = mean(ELA_T0),
+              Fv_Fm_value = mean(Fv_Fm_T0),
+              PI_abs_value = mean(PI_abs_T0))
+ df1 <- data.frame(plant_growth1)
+ dat1 <- rbind(dat1, df1)
+ plant_growth2 <- plant %>%
+   filter(Population_type == p) %>%
+   group_by(Soil_type) %>%
+   summarize(ELA_value = mean(ELA_T1),
+             Fv_Fm_value = mean(Fv_Fm_T1),
+             PI_abs_value = mean(PI_abs_T1))
+ df2 <- data.frame(plant_growth2)
+ dat2 <- rbind(dat2, df2)
+ plant_growth3 <- plant %>%
+   filter(Population_type == p) %>%
+   group_by(Soil_type) %>%
+   summarize(ELA_value = mean(ELA_T2),
+             Fv_Fm_value = mean(Fv_Fm_T2),
+             PI_abs_value = mean(PI_abs_T2))
+ df3 <- data.frame(plant_growth3)
+ dat3 <- rbind(dat3, df3)
+}
 
-# NM_PL35_P Calculate mean for ELA, FvFm, PI_abs
-pl35.growth1 <- plant %>% 
-  filter(Population_type == "NM_PL35_P") %>%
-  group_by(Soil_type) %>%
-  summarize(ELA_value = mean(ELA_T0),
-            Fv_Fm_value = mean(Fv_Fm_T0),
-            PI_abs_value = mean(PI_abs_T0))
+## Combine the 3 datasets, add days, and population_type
+plant.growth_time <- rbind(dat1, dat2)
+plant.growth_time <- rbind(plant.growth_time, dat3)
+plant.growth_time$days <- as.numeric(rep(c(1,129,178), each = 16))
+plant.growth_time$Population_type <- as.character(rep(c("M_PL22_P", "M_PL27_P", 
+                                                        "NM_PL14_P", "NM_PL35_P"),each = 4))
+colnames<- names(plant.growth_time)[2:4]
+for(i in 2:4) {
+    fig_growth <- ggplot(plant.growth_time, aes(x = days, y = plant.growth_time[,i])) +
+    geom_point(aes(color =  Soil_type), size = 1.5) +
+    geom_line(aes(color = Soil_type), size = 1) +
+    scale_color_manual(values = c("NM_PL14_S"="#2B3990",
+                                  "NM_PL35_S"="#5686C5",
+                                  "M_PL22_S" = "#E23E26",
+                                  "M_PL27_S" ="#F58776")) +
+    facet_wrap(~Population_type) + 
+    ylab(colnames[i-1])+
+    theme_bw() + 
+    ggtitle(colnames[i-1])+
+    theme(legend.position="bottom")
 
-pl35.growth2 <- plant %>% 
-  filter(Population_type == "NM_PL35_P") %>%
-  group_by(Soil_type) %>%
-  summarize(ELA_value = mean(ELA_T1),
-            Fv_Fm_value = mean(Fv_Fm_T1),
-            PI_abs_value = mean(PI_abs_T1))
+  ggsave(filename = paste0("Plots/PlantChem/PlantGrowthRate_", colnames[i-1], "_02242022.pdf"), 
+         plot = fig_growth,
+         width = 10,
+         height = 6,
+         units ="in")
+}
 
-pl35.growth3 <- plant %>% 
-  filter(Population_type == "NM_PL35_P") %>%
-  group_by(Soil_type) %>%
-  summarize(ELA_value = mean(ELA_T2),
-            Fv_Fm_value = mean(Fv_Fm_T2),
-            PI_abs_value = mean(PI_abs_T2))
-
-# M_PL22_P Calculate mean for ELA, FvFm, PI_abs
-pl22.growth1 <- plant %>% 
-  filter(Population_type == "M_PL22_P") %>%
-  group_by(Soil_type) %>%
-  summarize(ELA_value = mean(ELA_T0),
-            Fv_Fm_value = mean(Fv_Fm_T0),
-            PI_abs_value = mean(PI_abs_T0))
-
-pl22.growth2 <- plant %>% 
-  filter(Population_type == "M_PL22_P") %>%
-  group_by(Soil_type) %>%
-  summarize(ELA_value = mean(ELA_T1),
-            Fv_Fm_value = mean(Fv_Fm_T1),
-            PI_abs_value = mean(PI_abs_T1))
-
-pl22.growth3 <- plant %>% 
-  filter(Population_type == "M_PL22_P") %>%
-  group_by(Soil_type) %>%
-  summarize(ELA_value = mean(ELA_T2),
-            Fv_Fm_value = mean(Fv_Fm_T2),
-            PI_abs_value = mean(PI_abs_T2))
-
-# M_PL27_P Calculate mean for ELA, FvFm, PI_abs
-pl27.growth1 <- plant %>% 
-  filter(Population_type == "M_PL22_P") %>%
-  group_by(Soil_type) %>%
-  summarize(ELA_value = mean(ELA_T0),
-            Fv_Fm_value = mean(Fv_Fm_T0),
-            PI_abs_value = mean(PI_abs_T0))
-
-pl27.growth2 <- plant %>% 
-  filter(Population_type == "M_PL22_P") %>%
-  group_by(Soil_type) %>%
-  summarize(ELA_value = mean(ELA_T1),
-            Fv_Fm_value = mean(Fv_Fm_T1),
-            PI_abs_value = mean(PI_abs_T1))
-
-pl27.growth3 <- plant %>% 
-  filter(Population_type == "M_PL22_P") %>%
-  group_by(Soil_type) %>%
-  summarize(ELA_value = mean(ELA_T2),
-            Fv_Fm_value = mean(Fv_Fm_T2),
-            PI_abs_value = mean(PI_abs_T2))
-
-# NM_PL14_P Add Days and Population type
-pl14.growth_time <- rbind(pl14.growth1, pl14.growth2)
-pl14.growth_time <- rbind(pl14.growth_time, pl14.growth3)
-pl14.growth_time$days <- as.numeric(rep(c(1,129,178), each = 4))
-pl14.growth_time$Population <- as.character(rep(c("NM_PL14_P"),each = 12))
-
-# NM_PL35_P Add Days and Population type
-pl35.growth_time <- rbind(pl35.growth1, pl35.growth2)
-pl35.growth_time <- rbind(pl35.growth_time, pl35.growth3)
-pl35.growth_time$days <- as.numeric(rep(c(1,129,178), each = 4))
-pl35.growth_time$Population <- as.character(rep(c("NM_PL35_P"),each = 12))
-
-# M_PL22_P Add Days and Population type
-pl22.growth_time <- rbind(pl22.growth1, pl22.growth2)
-pl22.growth_time <- rbind(pl22.growth_time, pl22.growth3)
-pl22.growth_time$days <- as.numeric(rep(c(1,129,178), each = 4))
-pl22.growth_time$Population <- as.character(rep(c("M_PL22_P"),each = 12))
-
-# M_PL27_P Add Days and Population type
-pl27.growth_time <- rbind(pl27.growth1, pl27.growth2)
-pl27.growth_time <- rbind(pl27.growth_time, pl27.growth3)
-pl27.growth_time$days <- as.numeric(rep(c(1,129,178), each = 4))
-pl27.growth_time$Population <- as.character(rep(c("M_PL27_P"),each = 12))
-
-# Combine all four population type
-all_growth_time <- rbind(pl14.growth_time, pl35.growth_time)
-all_growth_time <- rbind(all_growth_time, pl22.growth_time)
-all_growth_time <- rbind(all_growth_time, pl27.growth_time)
-
-# Order Soil_type and Population_type
-all_growth_time$Soil_type <- factor(all_growth_time$Soil_type, 
-                                 levels = c("NM_PL14_S", "NM_PL35_S","M_PL22_S","M_PL27_S"))
-all_growth_time$Population <- factor(all_growth_time$Population, 
-                                  levels = c("NM_PL14_P", "NM_PL35_P","M_PL22_P","M_PL27_P"))
-
-# Plot leaf area over days
-pdf("Plots/PlantChem/PlantGrowthRate_ELA_02232022.pdf", width= 10, height= 6)
-ggplot(all_growth_time, aes(x = days, y = ELA_value)) +
-  geom_point(aes(color =  Soil_type), size = 1.5) +
-  geom_line(aes(color = Soil_type), size = 1) +
-  scale_color_manual(values = c("NM_PL14_S"="#2B3990",
-                                "NM_PL35_S"="#5686C5",
-                                "M_PL22_S" = "#E23E26",
-                                "M_PL27_S" ="#F58776")) +
-  facet_wrap(~Population) + 
-  theme_bw() +
-  theme(legend.position="bottom")
-dev.off()
-
-# Plot FV_Fm over days
-pdf("Plots/PlantChem/PlantGrowthRate_FvFm_02232022.pdf", width= 10, height= 6)
-ggplot(all_growth_time, aes(x = days, y = Fv_Fm_value)) +
-  geom_point(aes(color =  Soil_type), size = 1.5) +
-  geom_line(aes(color = Soil_type), size = 1) +
-  scale_color_manual(values = c("NM_PL14_S"="#2B3990",
-                                "NM_PL35_S"="#5686C5",
-                                "M_PL22_S" = "#E23E26",
-                                "M_PL27_S" ="#F58776")) +
-  facet_wrap(~Population) + 
-  theme_bw() +
-  theme(legend.position="bottom")
-dev.off()
-
-# Plot PI_abs over days
-pdf("Plots/PlantChem/PlantGrowthRate_PI_abs_02232022.pdf", width= 10, height= 6)
-ggplot(all_growth_time, aes(x = days, y = PI_abs_value)) +
-  geom_point(aes(color =  Soil_type), size = 1.5) +
-  geom_line(aes(color = Soil_type), size = 1) +
-  scale_color_manual(values = c("NM_PL14_S"="#2B3990",
-                                "NM_PL35_S"="#5686C5",
-                                "M_PL22_S" = "#E23E26",
-                                "M_PL27_S" ="#F58776")) +
-  facet_wrap(~Population) + 
-  theme_bw() +
-  theme(legend.position="bottom")
-dev.off()
 
